40a50311dc57a25b339d458a8cc32f182745ed42
Bihara Singh <rbihara100@gmail.com>
2021-06-23 03:00:39 +0530

    My java practical

--- helloWorld/src/Hello.java
+++ helloWorld/src/Hello.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class Hello {
 
     public static void main(String[] arg){
-        System.out.println("Hi My name is Bihara Singh");
+        System.out.println("Hi My name is Bihara");
 
     }
 }
